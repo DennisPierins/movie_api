87c7cf2fb900684e317e6037bc94623ba998759d
DennisPierins <dennispierins@gmail.com>
2020-11-10 14:22:46 +0100

    Heroku

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   //.catch(error => handleError(error));
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
