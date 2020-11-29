a56b204edced81496f77279ed7e87710ed90d394
DennisPierins <dennispierins@gmail.com>
2020-11-29 10:57:38 +0100

    CORS

--- index.js
+++ index.js
@@ -22,9 +22,22 @@ app.use(express.static('public'));
 
 app.use(bodyParser.json());
 
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://themyflixapi.herokuapp.com'];
+
 const { check, validationResult } = require('express-validator');
 
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed allowedOrigins
+      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+// app.use(cors());
 
 let auth = require('./auth')(app);
 
