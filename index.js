6d9fd659cf158c65215c7724ff24240441996b33
DennisPierins <dennispierins@gmail.com>
2020-11-29 16:06:08 +0100

    Enable JWT authentication for movies endpoint

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get("/", (req, res) => {
 });
 
 // GET a list of data about all movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
    Movies.find()
    .then((movies) => {
      res.status(201).json(movies);
