ce4ec1ca0ce2cd859ca874b02b7310b213ac546b
DennisPierins <dennispierins@gmail.com>
2020-11-30 18:26:10 +0100

    Added GET user data to endpoints

--- index.js
+++ index.js
@@ -149,6 +149,18 @@ app.get("/users", passport.authenticate('jwt', { session: false }), function (re
      });
  });
 
+ // GET data about a single user by username
+ app.get('/users/:Username', passport.authenticate('jwt', { session: false}), (req, res) => {
+   Users.findOne({ Username: req.params.Username })
+     .then((user) => {
+       res.json(user);
+   })
+   .catch((err) => {
+     console.error(err);
+     res.status(500).send('Error: ' + err);
+   });
+ });
+ 
  // PUT user information
  app.put('/users/:Username',
  [
